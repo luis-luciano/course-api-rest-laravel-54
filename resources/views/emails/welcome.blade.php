d435aa0e24bbcc55260c1a0b8f5164e3d8433f39
luis-luciano <alberto.rammss@gmail.com>
2018-08-29 19:36:10 -0500

    Se utiliza markdown vistas para el envio de emails

--- resources/views/emails/welcome.blade.php
+++ resources/views/emails/welcome.blade.php
@@ -1,4 +1,11 @@
-Hola {{ $user->name }}
+@component('mail::message')
+# Hola {{ $user->name }}
 Gracias por crear una nueva cuenta. Por favor verificala en el siguiente enlace.
 
-{{ route('verify',$user->verification_token) }}
+@component('mail::button', ['url' => route('verify',$user->verification_token)])
+Confirmar cuenta
+@endcomponent
+
+Gracias.<br>
+{{ config('app.name') }} 
+@endcomponent
